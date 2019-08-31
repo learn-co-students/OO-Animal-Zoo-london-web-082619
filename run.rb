94d53fabf60cbcce7ae9c1da6a1648ea2e10c7bd
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-08-31 21:07:49 +0100

    Done.

--- run.rb
+++ run.rb
@@ -5,6 +5,18 @@ require 'pry'
 
 #Test your code here
 
+lion = Animal.new("Lion", 100, "Simba")
+lion2 = Animal.new("Lion", 100, "Nala")
+tiger = Animal.new("Tiger", 75, "Tigger")
+bear = Animal.new("Black Bear", 225, "PoohBear")
+
+new_zoo1 = Zoo.new("London Zoo", "London")
+new_zoo2 = Zoo.new("Toronto Zoo", "Toronto")
+
+lion.assign_zoo(new_zoo1)
+lion2.assign_zoo(new_zoo2)
+tiger.assign_zoo(new_zoo2)
+bear.assign_zoo(new_zoo2)
 
 binding.pry
 puts "done"
