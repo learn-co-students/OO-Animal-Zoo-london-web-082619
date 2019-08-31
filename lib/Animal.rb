1543344543cf51e59738a7d251986efe08b64fe5
Angus Morrison <contact@angus-morrison.com>
2019-08-31 19:13:50 +0100

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,28 @@
 class Animal
+    attr_accessor :weight, :zoo
+    attr_reader :species, :nickname
+
+    @@all = []
+
+    def initialize(species, weight, nickname, zoo)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @zoo = zoo
+
+        @@all << self
+    end
+
+    ###### Class methods ######
+
+    #Return an array of all animal instances
+    def self.all()
+        @@all
+    end
+
+    #Return an array of all animals matching the give species
+    def self.find_by_species(species)
+        self.all().select() { | animal | animal.species == species }
+    end
 
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,47 @@
 class Zoo
+    attr_reader :name, :location
+
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+    ###### Instance methods ######
+
+    #Return an array of all the animals at the zoo
+    def animals()
+        Animal.all.select { | animal | animal.zoo() == self }
+    end
+
+    #Return all the species of animals in the zoo
+    def animal_species()
+        self.animals().map() { | animal | animal.species() }.uniq
+    end
+
+    #Return an array of animals at the zoo of a given species
+    def find_by_species(species)
+        self.animals().select() { | animal | animal.species == species }
+    end
+
+    #Return an array of all the nicknames of animals at the zoo
+    def animal_nicknames()
+        self.animals.map() { | animal | animal.nickname }
+    end
+
+    
+    ###### Class methods ######
+
+    #Return an array of all instances of Zoo
+    def self.all()
+        @@all
+    end
+
+    #Return an array of all zoos at the given location
+    def self.find_by_location(location)
+        self.all().select() { | zoo | zoo.location == location }
+    end
 
 end
