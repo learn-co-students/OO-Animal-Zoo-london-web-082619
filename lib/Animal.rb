90d243b7983eacd0d00a3b4aeb2b95526f0f2708
github username <github email address>
2019-09-02 00:20:39 +0100

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,22 @@
 class Animal
+    @@all = [] 
 
+attr_reader :species, :nickname
+attr_accessor :weight, :zoo
+
+def initialize(species,weight,nickname,zoo)
+    @species = species
+    @weight = weight 
+    @nickname = nickname
+    @zoo = zoo 
+    @@all<< self
+end
+#return arrays of all animals which are of that species so use select.() 
+#if we needed species only then we would use map. 
+        def self.find_by_species(species)
+        self.all.select{|species|animal.species == species}
+end
+def self.all 
+    @@all
+end
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,44 @@
 class Zoo
+attr_reader :name, :location 
+    @@all = [] 
+    
+    def initialize(name,location)
+    @name = name 
+    @location = location 
+    @@all << self
+    end
 
+    
+    def self.all 
+    @@all 
+    end
+# we iterate over animals class to find out all the animals in the zoo.
+     def animals #instance method soo zoo1.animal will give you all the animals
+     Animal.all.select{|animal| animal.zoo == self}
+     
+    end
+
+   
+   # will return an array of species in the zoo
+    def animal_species(species)
+       self.animals.map{|animal| animal.species }.uniq
+    end
+ #species what are we iterating over ?? return an array of particular species
+    def find_by_species(species)
+        self.all.select{|animal|animal.species == species}
+    end
+#return an array of nicknames use map
+    def animal_nicknames 
+        self.all.map{|names|names.nicknames}
+    end
+    # class method
+
+    #returns location of zoos in that location use map and do compare to location.
+    def self.find_by_location(location)
+
+        self.all.map{|location|location.zoo == location}
+
+    end
 end
+
+ 
